ec1db6c70c96405c9a6333b5db44d27c97933f5e
Barrett Clark <bclark@geoforce.com>
2012-08-21 19:38:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsIntro::Application.config.secret_token = 'beeee675f7a2475729115ef071de14e77a9a4aecbe22b9a583efcacfd33c8008c0d4a392c5b9ec6f35a351d55ef039577cafeaef0f60beb96539e1bb7083c088'
