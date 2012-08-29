68d5356552b9996e4942cd2d706a60880c5297b1
Barrett Clark <bclark@geoforce.com>
2012-08-28 20:18:49 -0500

    Harden the question flow

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,7 @@
 class Answer < ActiveRecord::Base
   attr_accessible :question_id, :sequence, :text, :value
   belongs_to :question
+  
+  validates_uniqueness_of :sequence, :scope => :question_id
+  validates_uniqueness_of :value, :scope => :question_id
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,6 @@ class Question < ActiveRecord::Base
   
   belongs_to :survey
   has_many :answers
+  
+  TYPES = [ 'radio', 'checkbox' ]
 end
